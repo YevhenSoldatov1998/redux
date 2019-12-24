3b278156793cb0bee441fae17ee4798bc61bd679
YevhenSoldatov1998 <arr.ess1998@gmail.com>
2019-12-24 18:37:23 +0300

    Updates

---
+++ precache-manifest.accc3d7133483821fb94e34bcb81bcb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "839f7200aa4cfe1232f8061c250468a2",
+    "url": "/redux/index.html"
+  },
+  {
+    "revision": "eb01da0b56a2ec129753",
+    "url": "/redux/static/css/main.bbbeee35.chunk.css"
+  },
+  {
+    "revision": "db83e6afae3c221f1d4c",
+    "url": "/redux/static/js/2.b995f7b2.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/redux/static/js/2.b995f7b2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eb01da0b56a2ec129753",
+    "url": "/redux/static/js/main.bd201ed5.chunk.js"
+  },
+  {
+    "revision": "66264f7e4b76472ab972",
+    "url": "/redux/static/js/runtime-main.d4753d46.js"
+  }
+]);
